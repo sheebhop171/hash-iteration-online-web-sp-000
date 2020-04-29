089d005a9bff771deb5adc791b5298f499ebfa3b
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-29 22:30:24 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,6 +9,7 @@
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |suite, name|
+    winner = " "
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end

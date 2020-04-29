637889e2db61442eeb88af478736e2c3dc89284e
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-29 22:33:45 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,9 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
   puts "Happy Birthday #{kids name}! You are now #{age} years old!"
 end
-
-
